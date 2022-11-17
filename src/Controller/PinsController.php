2f7898fd40fc3273623df6e55f3c98cf3874445d
gahette <gahette@icloud.com>
2022-11-17 13:24:12 +0100

    Add show page and navigation between pages

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +14,14 @@ class PinsController extends AbstractController
     public function index(PinRepository $pinRepository): Response
     {
         $pins = $pinRepository->findAll();
+
         return $this->render('pins/index.html.twig', compact('pins'));
+    }
+
+
+    #[Route('/pins/{id<[0-9]+>}', name: 'app_pins_show')]
+    public function show(Pin $pin): Response
+    {
+        return $this->render('pins/show.html.twig', compact('pin'));
     }
 }
